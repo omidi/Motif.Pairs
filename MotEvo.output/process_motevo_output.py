a25f537cbebe9c929a0dc6ffb6e35eb1620c95bc
omidi <saeed.omidi@gmail.com>
2016-02-22 14:23:05 +0100

    To the process_motevo_output a new feature is added that by receiving a file that contains the ID of regions of interest, it only produce the sitecounts concerning the regions of intrerests.

--- MotEvo.output/process_motevo_output.py
+++ MotEvo.output/process_motevo_output.py
@@ -21,28 +21,47 @@ def arguments():
                         type=float, required=False, default=0.,
                         help="""Cutoff over posteriors. Only values over the cutoff
                         will be added to the sitecount. Default value is 0.""")
+    parser.add_argument('-p', '--proxy', dest='proxyBED', action='store',
+                        type=str, required=False,
+                        help="""Optional BED file that contains regions of interest.
+                        Only sites within the regions of interest will be added to the
+                        final sitecount. Note that, if not provided, all regions will
+                        be considered to build the sitecounts.""")
     args = parser.parse_args()
     return args
 
 
-def makeSitecount(infile, outfile=None, cutoff=0.):
+def makeSitecount(infile, outfile=None, cutoff=0., proxy=None):
     """it takes the name of a MotEvo file and returns a dictionary with
     that contains the sum of posterior for each region
     """
     sitecounts = OrderedDict()  # used to remember the insertion order
-    with open(infile, 'r') as inf:
-        for rec in csv.reader(inf, delimiter='\t'):
-            region_name = rec[3].split(";")[-1]
-            sitecounts.setdefault(region_name, 0.)
-            post = float(rec[4])
-            if post > cutoff:
-                sitecounts[region_name] += post
+    if not proxy:    # if the proxy BED file is not provided
+        with open(infile, 'r') as inf:
+            for rec in csv.reader(inf, delimiter='\t'):
+                region_name = rec[3].split(";")[-1]
+                sitecounts.setdefault(region_name, 0.)
+                post = float(rec[4])
+                if post > cutoff:
+                    sitecounts[region_name] += post
+    else:
+        regions_of_interest = dict([(l.split()[4], 0) for l in open(proxy)])   # by default it' assumed that 5th
+                                                                               # column contains the region IDs
+        with open(infile, 'r') as inf:
+            for rec in csv.reader(inf, delimiter='\t'):
+                region_name = rec[3].split(";")[-1]
+                if not (region_name in regions_of_interest):
+                    continue
+                sitecounts.setdefault(region_name, 0.)
+                post = float(rec[4])
+                if post > cutoff:
+                    sitecounts[region_name] += post
     return sitecounts
 
 
 def main():
     args = arguments()
-    sitecounts = makeSitecount(args.input_file, args.output_file, args.cutoff)
+    sitecounts = makeSitecount(args.input_file, args.output_file, args.cutoff, args.proxyBED)
     if not args.output_file:
         for region, sitecount in sitecounts.items():
             print '\t'.join([
